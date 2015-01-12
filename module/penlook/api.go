5605e03a568334efebf42e451955009b4c703346
loint <loinguyentrung@gmail.com>
2015-01-12 15:02:09 +0700

    Add penlook api as service module

---
+++ module/penlook/api.go
@@ -0,0 +1,24 @@
+package penlook
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+func Api() {
+
+	router := gin.Default()
+	router.GET("/", func(c *gin.Context) {
+		c.String(200, "hello world")
+	})
+	router.GET("/ping", func(c *gin.Context) {
+		c.String(200, "pong")
+	})
+	router.POST("/submit", func(c *gin.Context) {
+		c.String(401, "not authorized")
+	})
+	router.PUT("/error", func(c *gin.Context) {
+		c.String(500, "and error hapenned :(")
+	})
+	router.Run(":80")
+
+}
