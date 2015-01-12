42ad7de0ad73352d1a7bf27f45b7f6b756011247
loint <loinguyentrung@gmail.com>
2015-01-12 16:43:00 +0700

    Remove port, not necessary for service module

--- api.go
+++ api.go
@@ -8,12 +8,10 @@ import (
 func main() {
 
 	service := daemon.Service{
-		Name:        "penlook-api",
+		Name:        "api",
 		Description: "Penlook API Service",
-		Port:        9876,
 		Process:     penlook.Api,
 	}
 
-	// Run service in daemon
-	service.Daemon()
+	service.Initialize()
 }
--- socket.go
+++ socket.go
@@ -10,10 +10,8 @@ func main() {
 	service := daemon.Service{
 		Name:        "socket",
 		Description: "Penlook Socket Service",
-		Port:        9876,
 		Process:     penlook.Socket,
 	}
 
-	// Run service in daemon
-	service.Daemon()
+	service.Initialize()
 }
