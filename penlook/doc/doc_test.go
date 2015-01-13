ea358ae17135f7e9d5197cd24fc7f67eccece489
loint <loint@penlook.com>
2015-01-14 00:28:42 +0700

    Fix author information

--- penlook/doc/doc_test.go
+++ penlook/doc/doc_test.go
@@ -1,7 +1,7 @@
 // Copyright 2014 Penlook Development Team. All rights reserved.
 // Use of this source code is governed by
 // license that can be found in the LICENSE file.
-// Author : loint@penlook.com
+// Author : Loi Nguyen <loint@penlook.com>
 
 package doc
 
