0c40b974e33656dfb36a7b6334380ceedaa57368
loint <loinguyentrung@gmail.com>
2015-01-13 13:23:00 +0700

    refactor structure

--- penlook.sh
+++ penlook.sh
@@ -1,3 +1,6 @@
 TMP=`pwd`
-cd module/penlook
-go test ./...
+cd $TMP/penlook/library
+go build github.com/penlook/service/penlook/library
+cd $TMP/penlook
+go build github.com/penlook/service/penlook
+cd $TMP
