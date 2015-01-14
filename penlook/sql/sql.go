227fc32e35dfa9c11a76b54f431cc686fb97766d
loint <loinguyentrung@gmail.com>
2015-01-14 16:08:25 +0700

    Add mysql for services

---
+++ penlook/sql/sql.go
@@ -0,0 +1,11 @@
+// Copyright 2014 Penlook Development Team. All rights reserved.
+// Use of this source code is governed by
+// license that can be found in the LICENSE file.
+// Author : Loi Nguyen <loint@penlook.com>
+
+package sql
+
+type Sql struct {
+	Name   string
+	Server []string
+}
