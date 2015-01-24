490e1696d28e2f3479063148bcae9630c0806308
tinntt <ntttin132@gmail.com>
2015-01-24 15:01:33 +0700

    Finished Get, Put, Delete, ListAllBucket function by using goamz

--- penlook/s3/s3_example.go
+++ penlook/s3/s3_example.go
@@ -4,28 +4,92 @@ import (
 	// "fmt"
 	"github.com/mitchellh/goamz/aws"
 	"github.com/mitchellh/goamz/s3"
+	"io/ioutil"
 	"log"
 )
 
 func main() {
+	// Put("penlook-abc", "/home/tinntt/228990_depotcache_1.csd", "sample2.txt", "text/plain")
+	// Delete("penlook-abc", "sample1.txt")
+	// resp := ListAllBucket()
+	// log.Print(fmt.Sprintf("%T %+v", resp.Buckets[0], resp.Buckets[0]))
+	Get("penlook-abc", "sample2.txt", "/home/tinntt/test.txt", "text/plain")
+}
+
+func Put(bucketName string, fileName string, objectName string, contType string) {
+	auth, err := aws.EnvAuth()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	if contType == "" {
+		contType = "text/plain"
+	}
+
+	client := s3.New(auth, aws.APNortheast)
+	bucket := client.Bucket(bucketName)
+
+	data, err := ioutil.ReadFile(fileName)
+	err = bucket.Put(objectName, data, contType, s3.BucketOwnerFull)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func Get(bucketName string, objectName string, fileName string, contType string) {
+	auth, err := aws.EnvAuth()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if contType == "" {
+		contType = "text/plain"
+	}
+
+	client := s3.New(auth, aws.APNortheast)
+	bucket := client.Bucket(bucketName)
+
+	data, err := bucket.Get(objectName)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = ioutil.WriteFile(fileName, data, 0666)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func Delete(bucketName string, objectName string) {
 	auth, err := aws.EnvAuth()
 	if err != nil {
 		log.Fatal(err)
 	}
 	client := s3.New(auth, aws.APNortheast)
-	bucket := client.Bucket("penlook-abc")
+	bucket := client.Bucket(bucketName)
 
-	data := []byte("Hello World!!")
-	err = bucket.Put("sample.txt", data, "text/plain", s3.BucketOwnerFull)
+	err = bucket.Del(objectName)
 	if err != nil {
 		log.Fatal(err)
-		// panic(err.Error())
 	}
-	// resp, err := client.ListBuckets()
+}
 
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
+func ListAllBucket() *s3.ListBucketsResp {
+	auth, err := aws.EnvAuth()
+	if err != nil {
+		log.Fatal(err)
+	}
 
-	// log.Print(fmt.Sprintf("%T %+v", resp.Buckets[0], resp.Buckets[0]))
+	client := s3.New(auth, aws.APNortheast)
+	resp, err := client.ListBuckets()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return resp
 }
