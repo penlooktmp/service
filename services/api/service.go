05de5799f8b3a2f52fedc5f37fd13af5a5e5f102
loint <loint@penlook.com>
2015-01-27 15:21:02 +0700

    Refactor services

--- services/api/service.go
+++ services/api/service.go
@@ -8,6 +8,9 @@ package main
 import (
 	"github.com/penlook/daemon"
 	"github.com/penlook/gin"
+	"log"
+	"net"
+	//"strconv"
 )
 
 func main() {
@@ -32,7 +35,49 @@ type User struct {
 }
 
 func LocalApi() {
-	// Local
+
+	// Set up listener for defined host and port
+	/*listener, err := net.Listen("tcp", ":"+strconv.Itoa(80))
+
+	if err != nil {
+		log.Println(err)
+	}
+
+	// set up channel on which to send accepted connections
+	listen := make(chan net.Conn, 100)
+	go acceptConnection(listener, listen)
+
+	for {
+		select {
+		case conn := <-listen:
+			go handleClient(conn)
+		}
+	}*/
+}
+
+// Accept a client connection and collect it in a channel
+func acceptConnection(listener net.Listener, listen chan<- net.Conn) {
+	log.Println("ACCEPT CONNECTION")
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			continue
+		}
+		listen <- conn
+	}
+}
+
+func handleClient(client net.Conn) {
+	log.Println("HANDLE CLIENT")
+	for {
+		buf := make([]byte, 4096)
+		numbytes, err := client.Read(buf)
+		if numbytes == 0 || err != nil {
+			return
+		}
+		log.Println(string(buf))
+		client.Write(buf)
+	}
 }
 
 func RemoteApi() {
