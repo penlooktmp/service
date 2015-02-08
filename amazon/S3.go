1046c8b6adc8c7247817ad805d7fc2c244f0bcf7
tinntt <ntttin132@gmail.com>
2015-02-08 17:59:08 +0700

    Create struct S3

--- amazon/S3.go
+++ amazon/S3.go
@@ -3,66 +3,78 @@ package main
 import (
 	"fmt"
 	"github.com/awslabs/aws-sdk-go/aws"
-	"github.com/awslabs/aws-sdk-go/gen/s3"
+	sdkS3 "github.com/awslabs/aws-sdk-go/gen/s3"
 	"os"
 )
+
+type S3 struct {
+	s3cli *sdkS3.S3
+}
 
 func main() {
 
 	var (
-		err         error
 		bucket      string = "penlook-abc" // change to your convenience
-		fd          *os.File
 		contenttype string = "binary/octet-stream"
 	)
 
-	creds := aws.Creds("AKIAJTPBBS46Y7SJETQQ", "Zc/5Ii8qnb19xKiBxgLnRG2DlrRNpdMyWN10WSlr", "")
-	cli := s3.New(creds, "ap-northeast-1", nil)
-
-	// open the file to upload
-	if len(os.Args) != 2 {
-		fmt.Printf("Usage: %s <inputfile>\n", os.Args[0])
-		os.Exit(1)
+	s3 := S3{
+		s3cli: S3Create(),
 	}
-	fi, err := os.Stat(os.Args[1])
+
+	s3.PutObject(bucket, "abc.txt", contenttype)
+	s3.ListObject(bucket)
+}
+
+func S3Create() *sdkS3.S3 {
+	creds := aws.Creds("AKIAJTPBBS46Y7SJETQQ", "Zc/5Ii8qnb19xKiBxgLnRG2DlrRNpdMyWN10WSlr", "")
+	cli := sdkS3.New(creds, "ap-northeast-1", nil)
+	return cli
+}
+
+func (s3 *S3) PutObject(bucketName string, fileName string, contenttype string) {
+	// open the file to upload
+	fi, err := os.Stat(fileName)
 	if err != nil {
 		fmt.Printf("Error: no input file found in '%s'\n", os.Args[1])
 		os.Exit(1)
 	}
-	fd, err = os.Open(os.Args[1])
+	fd, err := os.Open(fileName)
 	if err != nil {
 		panic(err)
 	}
 	defer fd.Close()
 
 	// create a bucket upload request and send
-	objectreq := s3.PutObjectRequest{
+	objectreq := sdkS3.PutObjectRequest{
 		ACL:           aws.String("public-read"),
-		Bucket:        aws.String(bucket),
+		Bucket:        aws.String(bucketName),
 		Body:          fd,
 		ContentLength: aws.Long(int64(fi.Size())),
 		ContentType:   aws.String(contenttype),
 		Key:           aws.String(fi.Name()),
 	}
-	_, err = cli.PutObject(&objectreq)
+	_, err = s3.s3cli.PutObject(&objectreq)
 	if err != nil {
 		fmt.Printf("Error: %v\n", err)
 	} else {
-		fmt.Printf("%s\n", "https://s3.amazonaws.com/"+bucket+"/"+fi.Name())
+		fmt.Printf("%s\n", "https://s3.amazonaws.com/"+bucketName+"/"+fi.Name())
 	}
+}
 
+func (s3 *S3) ListObject(bucketName string) {
 	// list the content of the bucket
-	listreq := s3.ListObjectsRequest{
-		Bucket: aws.StringValue(&bucket),
+	listreq := sdkS3.ListObjectsRequest{
+		Bucket: aws.StringValue(&bucketName),
 	}
-	listresp, err := cli.ListObjects(&listreq)
+	listresp, err := s3.s3cli.ListObjects(&listreq)
 	if err != nil {
 		panic(err)
 	}
 	if err != nil {
 		fmt.Printf("Error: %v\n", err)
 	} else {
-		fmt.Printf("Content of bucket '%s': %d files\n", bucket, len(listresp.Contents))
+		fmt.Printf("Content of bucket '%s': %d files\n", bucketName, len(listresp.Contents))
 		for _, obj := range listresp.Contents {
 			fmt.Println("-", *obj.Key)
 		}
