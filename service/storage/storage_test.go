6043a218d59497efc2f3e8eb80ce6c57342ca0bd
tinntt <ntttin132@gmail.com>
2015-03-08 14:01:05 +0700

    rewrite test travis

--- service/storage/storage_test.go
+++ service/storage/storage_test.go
@@ -39,9 +39,9 @@ var s3 = S3{}
 
 func TestS3Config(t *testing.T) {
 	assert := assert.New(t)
-	s3.GetConfig()
-	assert.NotNil(s3.ID)
-	assert.NotNil(s3.Secret)
+	// s3.GetConfig()
+	assert.NotNil(os.Getenv("AWS_S3_KEY"))
+	assert.NotNil(os.Getenv("AWS_S3_SECRET"))
 
 	assert.Equal(20, len(s3.ID))
 	assert.Equal(40, len(s3.Secret))
