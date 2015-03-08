4e3a64e89f0201dac61e5f305ab2ad5b859ab9c8
tinntt <ntttin132@gmail.com>
2015-03-08 11:42:41 +0700

    apply new code to storage service

--- service/storage/storage.go
+++ service/storage/storage.go
@@ -1,31 +1,3 @@
-/**
- * Penlook Project
- *
- * Copyright (c) 2015 Penlook Development Team
- *
- * --------------------------------------------------------------------
- *
- * This program is free software: you can redistribute it and/or
- * modify it under the terms of the GNU Affero General Public License
- * as published by the Free Software Foundation, either version 3
- * of the License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU Affero General Public License for more details.
- *
- * You should have received a copy of the GNU Affero General Public
- * License along with this program.
- * If not, see <http://www.gnu.org/licenses/>.
- *
- * --------------------------------------------------------------------
- *
- * Author:
- *     Loi Nguyen       <loint@penlook.com>
- *     Tin Nguyen       <tinntt@penlook.com>
- */
-
 package main
 
 import (
@@ -35,9 +7,15 @@ import (
 	sdkS3 "github.com/awslabs/aws-sdk-go/gen/s3"
 	"github.com/penlook/service/module/redis"
 	"io/ioutil"
+	"log"
+	"net"
 	"os"
+	"os/signal"
 	"strings"
+	"syscall"
 )
+
+var stdlog, errlog *log.Logger
 
 type S3 struct {
 	s3cli  *sdkS3.S3
@@ -154,5 +132,66 @@ func (s3 *S3) DeleteObject(bucketName string, fileName string) {
 	}
 	if err != nil {
 		fmt.Printf("Error: %v\n", err)
+	}
+}
+
+func S3Service() {
+	s3 := S3{}
+	s3.GetConfig()
+	s3.Create()
+
+	interrupt := make(chan os.Signal, 1)
+	signal.Notify(interrupt, os.Interrupt, os.Kill, syscall.SIGTERM)
+
+	// Set up listener for defined host and port
+	listener, err := net.Listen("tcp", ":9977")
+	if err != nil {
+		//return "Possibly was a problem with the port binding", err
+		fmt.Println("Possibly was a problem with the port binding")
+	}
+
+	// set up channel on which to send accepted connections
+	listen := make(chan net.Conn, 100)
+	go acceptConnection(listener, listen)
+
+	// loop work cycle with accept connections or interrupt
+	// by system signal
+	for {
+		select {
+		case conn := <-listen:
+			s3.PutObject("penlook-abc", "/home/tinntt/src/github.com/penlook/service/LICENSE.md", "text/plain")
+			go handleClient(conn)
+		case killSignal := <-interrupt:
+			stdlog.Println("Got signal:", killSignal)
+			stdlog.Println("Stoping listening on ", listener.Addr())
+			listener.Close()
+			if killSignal == os.Interrupt {
+				fmt.Println("Daemon was interruped by system signal")
+				//return "Daemon was interruped by system signal", nil
+			}
+			fmt.Println("Daemon was killed")
+			//return "Daemon was killed", nil
+		}
+	}
+}
+
+func acceptConnection(listener net.Listener, listen chan<- net.Conn) {
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			continue
+		}
+		listen <- conn
+	}
+}
+
+func handleClient(client net.Conn) {
+	for {
+		buf := make([]byte, 4096)
+		numbytes, err := client.Read(buf)
+		if numbytes == 0 || err != nil {
+			return
+		}
+		client.Write(buf)
 	}
 }
